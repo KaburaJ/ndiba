49178f79aa05d278de3218fad82aea77e78e35da
KaburaJ <kaburajoan25@gmail.com>
2024-10-14 03:00:03 +0300

    first

--- ndibaAdmin/src/firebase.js
+++ ndibaAdmin/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database'; // Import for Realtime Database
 
 const firebaseConfig = {
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    databaseURL: REACT_APP_DATABASE_URL,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket: REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-    appId: REACT_APP_APP_ID
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`
 };
 
 // Initialize Firebase
