9ccc78dbe692b51b5d210259fdcc22d3ad54029c
KaburaJ <kaburajoan25@gmail.com>
2024-12-04 17:56:47 +0300

    image upload updated 111

--- ndibaAdmin/src/components/ProjectsCard/ProjectsCard.jsx
+++ ndibaAdmin/src/components/ProjectsCard/ProjectsCard.jsx
@@ -71,7 +71,7 @@ const EditModal = ({ cardData, onSave, onCancel }) => {
                   })
                     .then((response) => response.json())
                     .then((data) => {
-                        setNewData({ ...newData, image: formData.data.secure_url})
+                        setNewData({ ...newData, image: formData.data})
                         setPreviewUrl({ ...newData, image: formData.data.secure_url}); // Set the preview URL to the uploaded image or video'
                         console.log(formData.data?formData.data.secure_url:null)
 
